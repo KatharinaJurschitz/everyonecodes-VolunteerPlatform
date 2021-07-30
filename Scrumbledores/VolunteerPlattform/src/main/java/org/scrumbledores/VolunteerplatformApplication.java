2e0db1bac467ed7ea0688d7ada55d58a72d54b1b
Peter Frimberger <peter.frimberger@gmail.com>
2021-07-30 10:13:32 +0200

    create project

---
+++ Scrumbledores/VolunteerPlattform/src/main/java/org/scrumbledores/VolunteerplatformApplication.java
@@ -0,0 +1,13 @@
+package org.scrumbledores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteerplatformApplication.class, args);
+	}
+
+}
---
+++ Scrumbledores/VolunteerPlattform/src/test/java/org/scrumbledores/VolunteerplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scrumbledores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
