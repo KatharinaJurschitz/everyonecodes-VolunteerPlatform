484384db38bc7800abc6a5a3d3abc6f0759f7099
Mike Zeplichal <mike.zeplichal@hotmail.com>
2021-08-04 15:24:32 +0200

    Added Endpoint Test and Service Tests

--- Scrumbledores/VolunteerPlattform/src/test/java/org/scrumbledores/UserServiceTest.java
+++ Scrumbledores/VolunteerPlattform/src/test/java/org/scrumbledores/UserServiceTest.java
@@ -1,24 +1,93 @@
 package org.scrumbledores;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
+import org.scrumbledores.user.PlatformUser;
 import org.scrumbledores.user.PlatformUserRepository;
 import org.scrumbledores.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.Set;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class UserServiceTest {
     @Autowired
     UserService service;
 
+    @MockBean
     PlatformUserRepository repository;
+
+
     PasswordEncoder encoder;
     Set<String> roles;
+
+    @Test
+    void testCreateUserSuccessful() {
+        PlatformUser platformUser = new PlatformUser("test", "test", Set.of("ROLE_VOLUNTEER"), "test", LocalDate.now(), "test", "scrumbledore.email@gmail.com", "test");
+        Mockito.when(repository.save(platformUser)).thenReturn(platformUser);
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertEquals(platformUser, result.get());
+        Mockito.verify(repository).save(platformUser);
+    }
+
+    @Test
+    void testCreateUserFailBecauseEmail() {
+        PlatformUser platformUser = new PlatformUser("test!!", "test", Set.of("ROLE_VOLUNTEER"), "test", LocalDate.now(), "test", "test@email.com", "test");
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testCreateUserFailBecauseUsername() {
+        PlatformUser platformUser = new PlatformUser("test!!", "test", Set.of("ROLE_VOLUNTEER"), "test", LocalDate.now(), "test", "test", "test");
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testCreateUserFailBecauseRoleSizeMoreThanOne() {
+        PlatformUser platformUser = new PlatformUser("test", "test", Set.of("ROLE_VOLUNTEER", "ROLE_INDIVIDUAL"), "test", LocalDate.now(), "test", "test", "test");
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testCreateUserFailBecauseRoleSizeEmpty() {
+        PlatformUser platformUser = new PlatformUser("test", "test", Set.of(), "test", LocalDate.now(), "test", "test", "test");
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testCreateUserFailBecauseFalseRole() {
+        PlatformUser platformUser = new PlatformUser("test", "test", Set.of("ROLE_TEST"), "test", LocalDate.now(), "test", "test", "test");
+        Optional<PlatformUser> result = service.createUser(platformUser);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void testFindUserSuccess() {
+        PlatformUser platformUser = new PlatformUser("test", "test", Set.of("ROLE_TEST"), "test", LocalDate.now(), "test", "test", "test");
+        Principal principal = mock(Principal.class);
+        Mockito.when(principal.getName()).thenReturn("test");
+        Mockito.when(repository.findOneByUsername("test")).thenReturn(Optional.of(platformUser));
+        PlatformUser result = service.findUser(principal);
+        Assertions.assertEquals(platformUser, result);
+        Mockito.verify(principal).getName();
+        Mockito.verify(repository).findOneByUsername("test");
+    }
+    
 
     @ParameterizedTest
     @CsvSource({
