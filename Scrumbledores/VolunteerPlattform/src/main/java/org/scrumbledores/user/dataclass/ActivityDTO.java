92b91e8a73aa24d5ea3e776ca60a37808ca315fc
Katharina Jurschitz <kjurschitz@gmail.com>
2021-08-16 16:46:14 +0200

    Edit and Post a drafted activity

--- Scrumbledores/VolunteerPlattform/src/main/java/org/scrumbledores/user/dataclass/ActivityDTO.java
+++ Scrumbledores/VolunteerPlattform/src/main/java/org/scrumbledores/user/dataclass/ActivityDTO.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,7 +20,8 @@ public class ActivityDTO {
     @NotEmpty
     private String description;
     private String recommendedSkills;
-    private List<String> categories;
+    private List<String> categories = new ArrayList<>();
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime startDate;
     private LocalDateTime endDate;
